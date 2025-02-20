88f356241a383baa10a746b2fb1d770b11cc0acc
NeoByteX <160131789+NeoByteXx@users.noreply.github.com>
2025-02-20 19:29:28 +0100

    Update menu-reset.js

--- themes/book/assets/menu-reset.js
+++ themes/book/assets/menu-reset.js
@@ -1,7 +1,7 @@
 (function() {
   var menu = document.querySelector("aside .book-menu-content");
   addEventListener("beforeunload", function(event) {
-      localStorage.setItem("menu.scrollTop", menu.scrollTop);
+      localStorage.setItem("menu.scrollTop", menu.scrollTop || 0);
   });
   menu.scrollTop = localStorage.getItem("menu.scrollTop");
 })();
